8151ca521e640f9019d3ecaf0022b878051f3bc6
szuyuc <szuyuc@andrew.cmu.edu>
2020-02-09 23:34:09 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Recipe from './Recipe';
 
-function App() {
+
+const App = () => {
+
+  const APP_ID = 'dd6159ee'
+  const APP_KEY = '876bd096218ac193616a9ac7eff649d3'
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('chicken');
+  // useEffect rerender our page but you can set it to run once based on the condition(value changes)
+  // useEffect(()=>{ console.log('effect running');},[]); //arr is for when to set it run 
+
+  useEffect(() => { getrecipes(); }, [query]);
+
+  // any external request you need to add async, because you dont know when the data coming back
+  const getrecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    //console.log(data);
+    setRecipes(data.hits);
+    console.log(data.hits);
+  }
+
+  // add event on it: everytime you trigger onChange you will get the event
+  const updateSearch = e => {
+    setSearch(e.target.value); // whatever you type into the input, the state changes
+    //console.log(search)
+  }
+
+  const getSearch = e => {
+    //stop the page refresh
+    e.preventDefault();
+    setQuery(search);
+    setSearch("");
+  }
+  // onchanges: input changes, it's going to run
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={getSearch} className="search-form">
+        <input className="search-bar" type="text" value={search} onChange={updateSearch} />
+        <button className="search-button" type="submit"> search</button>
+      </form>
+      {recipes.map(recipe => (<Recipe
+        title={recipe.recipe.label} calories={recipe.recipe.calories} image={recipe.recipe.image} key={recipe.recipe.label} ingredients={recipe.recipe.ingredients} />))}
     </div>
   );
+
 }
 
 export default App;
